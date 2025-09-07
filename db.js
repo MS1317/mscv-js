114f076ea70e45025ce4ab325a61627539b8d84d
ms1317 <manpreetsingh76732@gmail.com>
2025-09-07 13:25:21 +0530

    added ssl authorization

--- db.js
+++ db.js
@@ -6,6 +6,9 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  ssl: {
+    rejectUnauthorized: true,
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
