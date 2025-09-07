b10881b9f1cf48a18c426dbbb279b9a59e57e239
ms1317 <manpreetsingh76732@gmail.com>
2025-09-07 14:05:52 +0530

    databse changes

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
-import mysql from 'mysql2/promise';
 import 'dotenv/config';
+import pg from 'pg';
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: {
-    rejectUnauthorized: true,
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
 
 export default pool;
