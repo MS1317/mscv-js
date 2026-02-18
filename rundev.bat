a427839f55dbb7df3c40bbdb687d4d276c5fcaf7
ms1317 <manpreetsingh76732@gmail.com>
2026-02-18 18:33:31 +0530

    feat: Add undev.bat, unextra.bat, and unserver.bat for project execution and track them by updating .gitignore.

---
+++ rundev.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\manpr\OneDrive - TechiTuber\Desktop\Dev\JS\mscv
+npm run dev
+pause
---
+++ runextra.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d C:\Users\manpr\OneDrive - TechiTuber\Desktop\Dev\JS\mscv
+npm -v
+node -v
+pause
